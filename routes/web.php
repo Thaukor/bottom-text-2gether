2557af1ff1eb4c1afc3bc648308fcf35cc59aefc
Thaukor <claudio.carcamo@pregrado.uoh.cl>
2022-06-12 18:07:18 -0400

    Add database connection on profile page

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 // Controllers
 use App\Http\Controllers\TestController;
+use App\Http\Controllers\UserScheduleController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,8 +21,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/profile', [TestController::class, 'test'])->name('profile');
+//Route::get('/profile', [UserScheduleController::class, 'index'])->name('profile');
 
+Route::resource('/profile', UserScheduleController::class)->only(['index'])->missing(function (Request $request) {
+    return Redirect::route('profile.index');
+});
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');

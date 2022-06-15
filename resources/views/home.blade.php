b0aab2eb9ad8151c52839fca517209c9ce126f61
Thaukor <claudio.carcamo@pregrado.uoh.cl>
2022-06-14 21:57:28 -0400

    Fix modal locking up home screen

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,29 +9,30 @@
     <div class="locationviewer">
         <h4 id="txt_well">¿Cuál es tu destino?</h4>
         <input type="text" class="textstyle" id="txt1" placeholder="Tu destino...">
-        <div class="text-center mt-2">
-            <button class="btn btn-success" data-bs-toggle="modal" data-bs-target="#lookForGroupModal">Buscar grupos</button>
-        </div>
-        <div class="modal fade" id="lookForGroupModal" tabindex="-1" aria-labelledby="lookForGroupModal" aria-hidden="true">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="lookForGroupModalLabel">Buscar grupo</h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                        ...
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                        <button type="button" class="btn btn-primary">Save changes</button>
-                    </div>
-                </div>
+        <div class="mt-2">
+            <div class="text-center">
+                <button class="btn btn-success" data-bs-toggle="modal" data-bs-target="#lookForGroupModal">Buscar grupos</button>
             </div>
         </div>
         
     </div>
-    
+    <div class="modal" id="lookForGroupModal" tabindex="-1" aria-labelledby="lookForGroupModal" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="lookForGroupModalLabel">Buscar grupo</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    ...
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+            </div>
+        </div>
+    </div>
     <div class="d-flex justify-content-center h-100">
         
     </div>
